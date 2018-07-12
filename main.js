c9113231048968578bf2b603ba0def74679277fa
rodalcala <37354241+rodalcala@users.noreply.github.com>
2018-07-12 21:26:55 +1000

    Update main.js
    Tenemos hasta la tercera carta del usuario completado.

--- main.js
+++ main.js
@@ -47,4 +47,30 @@ function blackjack() {
 
   console.log(plHand());
   console.log(plScore());
+
+  function thirdCard() {
+    let third = window.confirm('Would you like another card?')
+    if (third) {
+      let thirdPl = Math.floor(Math.random() * 12) + 1;
+      plCards.push(thirdPl);
+    }
+    return plCards;
+  }
+
+  console.log(thirdCard());
+  console.log(plScore());
+
+  // Aca creo que quiero hacer una recursive function para que a partir
+  // de la cuarta carta como que haga un loop preguntando si quiere otra
+  // carta hasta que diga que no o el score >= 21;
+
+  // if (plScore() > 21) {
+  //   console.log("You've lost!")
+  // } else if (plScore() === 21) {
+  //   console.log("You've got 21!")
+  // } else {
+  //   while (plScore() < 21) {
+  //
+  //   }
+  // }
 }
