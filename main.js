e5b5f64dc4457ce127ddf21fb4cf50823e4eb5fe
rodalcala <37354241+rodalcala@users.noreply.github.com>
2018-07-16 14:15:19 +1000

    Update main.js
    Cambiamos el enfoque:
    *Las variables ahora son globales y las funciones son mas cortas;
    *Ya no tenemos una gran funcion que lo engloba todo sino que hay basicamente 4 steps:
      +Primer carta del DL;
      +Primeras dos cartas del PL;
      +Juego del PL;
      +Juego del DL (si es que juega);
    *Trate de comentar mejor el codigo para que se entienda;

--- main.js
+++ main.js
@@ -1,152 +1,164 @@
-function blackjack() {
-  let dlCards = [];
-  let plCards = [];
-  let dlScore = function() {
-    let score = 0;
-    for(let i = 0; i < dlCards.length; i++) {
-      if (dlCards[i] === 10 || dlCards[i] === 11 || dlCards[i] === 12) {
-        score += 10;
-      } else if (dlCards[i] === 1) {
-        score += 11;
-      } else {
-        score += dlCards[i];
-      }
-    }
-    for(let i = 0; i < dlCards.length; i++) {
-      if (score > 21 && dlCards[i] === 1) {
-        console.log('fix As');
-        score -= 10;
-        // for loop para solucionar el valor del As y que cambie de 11 a 1
-        // segun la ocacion
-      }
-    }
-    return score;
-  };
-  let plScore = function() {
-    let score = 0;
-    for(let i = 0; i < plCards.length; i++) {
-      if (plCards[i] === 10 || plCards[i] === 11 || plCards[i] === 12) {
-        score += 10;
-      } else if (plCards[i] === 1) {
-        score += 11;
-      } else {
-        score += plCards[i];
-      }
-    }
-    for (var i = 0; i < plCards.length; i++) {
-      if (score > 21 && plCards[i] === 1) {
-        console.log("fix As");
-        score -= 10;
-      }
-    }
-    return score;
-  };
-
-  // Hasta aca definimos las variables que van a contener las cartas de cada
-  // jugador y los puntajes de cada uno (que dependen de las cartas)
-
-  function dlFirstCard() {
-    let firstDl = Math.floor(Math.random() * 12) + 1;
-    dlCards.push(firstDl);
-    return firstDl;
-  }
-
-  console.log(dlFirstCard());
-  console.log(dlScore());
-
-  // Generamos y mostramos la primer carta de DL;
-
-  function plHand() {
-    let firstPl = Math.floor(Math.random() * 12) + 1;
-    let secondPl = Math.floor(Math.random() * 12) + 1;
-    plCards.push(firstPl);
-    plCards.push(secondPl);
-    return plCards;
-  }
-
-  console.log(plHand());
-  console.log(plScore());
-
-  // Generamos y mostramos las dos primeras cartas de PL;
-
-  function plGame() {
-    if (plScore() === 21) {
-      console.log('Blackjack!')
+let dlCards = [];
+let plCards = [];
+let dlScore = function() {
+  let score = 0;
+  for (let i = 0; i < dlCards.length; i++) {
+    if (dlCards[i] === 10 || dlCards[i] === 11 || dlCards[i] === 12) {
+      score += 10;
+    } else if (dlCards[i] === 1) {
+      score += 11;
     } else {
-      function plNextCard() {
-        if (plScore() > 21) {
-          console.log("You've lost.")
-        } else if (plScore() === 21) {
-          console.log('You made it to 21! lets see...')
-        } else {
-          // let next = window.confirm('Would you like another card?')
-          // if (next) {
-          let anotherCard = undefined;
-          // Hacemos que cada vez que se ejecute la funcion el valor de la
-          // variable anotherCard vuelva a undefined para que no se genere
-          // un loop infinito de sacar cartas hasta que plScore() > 21;
-          var hitButton = document.getElementById('hit');
-          hitButton.addEventListener('click', function next() {
-            anotherCard = true;
-            }
-          )
-          var standButton = document.getElementById('stand');
-          standButton.addEventListener('click', function next() {
-            anotherCard = false;
-            }
-          )
-          if (anotherCard) {
-            let nextPl = Math.floor(Math.random() * 12) + 1;
-            console.log(nextPl);
-            plCards.push(nextPl);
-            console.log(plScore())
-            plNextCard();
-          }
-          // }
-        }
-        return plCards;
-      }
-      console.log(plNextCard())
+      score += dlCards[i];
     }
-    return plCards;
   }
+  for (let i = 0; i < dlCards.length; i++) {
+    if (score > 21 && dlCards[i] === 1) {
+      console.log('fix As');
+      score -= 10;
+      // for loop para solucionar el valor del As y que cambie de 11 a 1
+      // segun la ocacion
+    }
+  }
+  return score;
+};
+let plScore = function() {
+  let score = 0;
+  for (let i = 0; i < plCards.length; i++) {
+    if (plCards[i] === 10 || plCards[i] === 11 || plCards[i] === 12) {
+      score += 10;
+    } else if (plCards[i] === 1) {
+      score += 11;
+    } else {
+      score += plCards[i];
+    }
+  }
+  for (var i = 0; i < plCards.length; i++) {
+    if (score > 21 && plCards[i] === 1) {
+      console.log("fix As");
+      score -= 10;
+    }
+  }
+  return score;
+};
 
-  console.log(plGame());
-  console.log(plScore());
+// Hasta aca definimos las variables que van a contener las cartas de cada
+// jugador y los puntajes de cada uno (que dependen de las cartas)
 
-  // Buenooooo, parece que me salio la recursive function!
-  // Con esto terminariamos la mano del PL y empezariamos la del DL.
+function dlFirstCard() {
+  let firstDl = Math.floor(Math.random() * 12) + 1;
+  // generamos la primer carta del DL
+  dlCards.push(firstDl);
+  // la agregamos a la mano del DL
+}
 
-  function dlGame() {
+function plHand() {
+  let firstPl = Math.floor(Math.random() * 12) + 1;
+  let secondPl = Math.floor(Math.random() * 12) + 1;
+  // generamos la dos primeras cartas del PL
+  plCards.push(firstPl);
+  plCards.push(secondPl);
+  // las agragamos a la mano del PL
+}
+
+let anotherCard = undefined;
+// esta variable va a cambiar segun el PL quiera otra carta o no.
+var hitButton = document.getElementById('hit');
+hitButton.addEventListener('click', function next() {
+  anotherCard = true;
+  // cambiamos el valor de anotherCard para pedir otra carta y
+  // ejecutamos plGame();
+  plGame();
+})
+var standButton = document.getElementById('stand');
+standButton.addEventListener('click', function next() {
+  anotherCard = false;
+  // cambiamos el valor de anotherCard para no pedir mas cartas y
+  // ejecutamos dlGame() ya que el turno del PL se saltea;
+  dlGame();
+})
+
+function plGame() {
+  // Si se activa esta funcion es porque pedimos sacar una carta nueva
+  if (anotherCard) {
+    let nextPl = Math.floor(Math.random() * 12) + 1;
+    // la generamos
+    console.log(nextPl);
+    // la imprimimos
+    plCards.push(nextPl);
+    // la sumamos a la mano del PL
+    console.log(plCards)
+    console.log(plScore())
+    // imprimimos la mano y el puntaje
+    anotherCard = undefined;
+    // y seteamos la variable anotherCard de nuevo a undefined para que
+    // el PL pueda elegir nuevamente entre 'hit' y 'stand' de ser necesario
     if (plScore() > 21) {
-      console.log('The house wins.');
-    } else {
-      let nextDl = Math.floor(Math.random() * 12) + 1;
-      console.log(nextDl);
-      dlCards.push(nextDl);
-      if (dlScore() === 21 && dlCards.length === 2) {
-        console.log('The house scores a blackjack and wins!');
-        // Si DL hace blackjack gana;
-      } else if (plScore() === 21 && plCards.length === 2) {
-        console.log('The players blackjack takes the game!')
-        // Si PL hace blackjack y DL no, se termina la jugada de DL con
-        // su segunda carta y no es necesario que siga jugando
-      } else if (dlScore() > 21) {
-        console.log('The house loses!');
-      } else if (dlScore() <= 21 && dlScore() >= plScore()) {
-        console.log(dlScore())
-        console.log('The house wins!');
-      } else if (dlScore() < 21 && dlScore() < plScore()) {
-        console.log(dlScore())
-        dlGame();
-      }
+      // Apagamos los botones de 'hit' y 'stand' y se termina la partida.
+      console.log("You've lost.")
+    } else if (plScore() === 21) {
+      console.log('You made it to 21! lets see...')
+      // Ejecutamos el juego del DL.
+      dlGame();
     }
-    return dlCards;
   }
+}
 
-  console.log(dlGame());
+function dlGame() {
+  // Una vez que se ejecuta esta funcion habria que "apagar" los botones
+  // de pedir carta y de plantarse porque el usuario ya no puede interactuar.
+  if (plScore() > 21) {
+    console.log('The house wins.');
+  } else {
+    let nextDl = Math.floor(Math.random() * 12) + 1;
+    console.log(nextDl);
+    dlCards.push(nextDl);
+
+    if (dlScore() === 21 && dlCards.length === 2) {
+      console.log('The house scores a blackjack and wins!');
+      // Si DL hace blackjack gana;
+    } else if (plScore() === 21 && plCards.length === 2) {
+      console.log('The players blackjack takes the game!')
+      // Si PL hace blackjack y DL no, se termina la jugada de DL con
+      // su segunda carta y no es necesario que siga jugando
+    } else if (dlScore() > 21) {
+      console.log('The house loses!');
+    } else if (dlScore() <= 21 && dlScore() >= plScore()) {
+      console.log(dlScore())
+      console.log('The house wins!');
+    } else if (dlScore() < 21 && dlScore() < plScore()) {
+      console.log(dlScore())
+      dlGame();
+      // Aguanten las funciones recursivas chabonnnnn
+    }
+  }
+  console.log(dlCards);
   console.log(dlScore());
+}
 
-  // Parece que el dlGame esta funcionando!
-  // Aguanten las funciones recursivas chabonnnnn
+function startGame() {
+  dlCards = [];
+  plCards = [];
+  // Seteamos las manos de los jugares a arrays vacios para empezar
+  dlFirstCard();
+  console.log(dlCards);
+  console.log(dlScore());
+  // Ejecutamos la funcion que determina la primer carta del DL e imprimimos
+  // cual es y que puntaje consigue.
+  plHand();
+  console.log(plCards);
+  console.log(plScore());
+  // Ejecutamos la funcion que determina las dos primeras cartas del PL e
+  // imprimimos cuales son y que puntaje consigue.
+  if (plScore() === 21) {
+    console.log('Blackjack!');
+    // Si de entrada PL saca un Blackjack lo imprimimos en pantalla e
+    // inmediatamente ejecutamos la jugada del DL para ver si saca
+    // un blackjack tambien.
+    dlGame();
+  }
+}
+
+var playButton = document.getElementById('playAgain');
+playButton.addEventListener('click', function next() {
+  startGame();
 }
